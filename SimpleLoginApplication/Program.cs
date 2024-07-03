593fd059a75f4080e2d377c500e9dc2cbeeeaede
Ameer Ahmed <aahmed335467@gmail.com>
2024-07-03 17:38:23 +0100

    Initial version for application

--- SimpleLoginApplication/Program.cs
+++ SimpleLoginApplication/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string user_name = "test123";
+string password = "extremepass321";
+
+string title_string = $"""
+    * * * * * * * * * * * * * * * * * * * * * 
+    *                                       *
+    *    -      ---    ---   ---  -    -    *
+    *    |     |   |  |       |   | \  |    *
+    *    |     |   |  |  --   |   |  \ |    *
+    *    ---    ---    ---   ---  -    -    *
+    *                                       *
+    * * * * * * * * * * * * * * * * * * * * * 
+    """;
+Console.WriteLine(title_string + "\n");
+
+while (true)
+{
+    Console.Write($"Please enter your user_name: ");
+    string user_input = Console.ReadLine();
+    Console.Write($"Please enter your password: ");
+    string user_pass = Console.ReadLine();
+    if (user_input == user_name && user_pass == password)
+    {
+        Console.WriteLine($"\nCredentials accepted. Welcome back {user_name}!");
+        break;
+    }
+    else if (user_input == user_name)
+    {
+        Console.WriteLine("\nError, that password does not seem to be associated with this account. Please try again.\n");
+    }
+    else 
+    {
+        Console.WriteLine("\nIncorrect login details. Please try again.\n");
+    }
+}
